b093b92bfc42f49fd2c7124dc90cee9aa4fc24dc
Osvaldas Valutis <osvaldas.v@gmail.com>
2022-10-26 20:15:54 +0300

    wip

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function enableAutoEventTracking({
+  attributeName = `tracker`,
+  sourceNode = document,
+  targets = [`a`, `button`],
+  trackers = [`plausible`],
+}) {
+
+
+  return () => {
+
+  }
+}
+
+function trackEvent({ trackers }) {
+
+}
+
+export { enableAutoEventTracking, trackEvent }
